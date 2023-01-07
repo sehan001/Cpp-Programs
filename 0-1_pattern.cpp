6cb0a0e0a63e070d2d0ec8cbc07bd04b143a1fa8
Mohammed Sehan <2000030708@kluniversity.in>
2023-01-07 22:40:58 +0530

    Create 0-1_pattern.cpp

---
+++ 0-1_pattern.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int n,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if((i+j)%2==0)
+            {
+            	cout<<1<<" ";
+            }
+            else
+            {
+            	cout<<0<<" ";
+            }
+        }
+        cout<<" \n";
+    }
+    
+    return 0;
+}
