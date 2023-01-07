4e42673ba55f9499689945654214827d8435c301
Mohammed Sehan <2000030708@kluniversity.in>
2023-01-07 23:07:05 +0530

    Create rhombus_pattern_using_star.cpp

---
+++ rhombus_pattern_using_star.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int n,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+    	for(j=1;j<=n-i;j++)
+    	{
+    		cout<<" ";
+    	}
+    	for(j=1;j<=n;j++)
+    	{
+    		cout<<"*"<<" ";
+    	}
+    	cout<<"\n";
+	}
+	return 0;
+}
